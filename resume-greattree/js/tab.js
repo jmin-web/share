299d01df0fb7cd0aaa47b03d3ec56e9a211296d8
chieh-min <chiehminchuang@gmail.com>
2025-03-21 11:10:50 +0800

    Add files via upload

--- resume-greattree/js/tab.js
+++ resume-greattree/js/tab.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-    $(".tab").click(function () {
-        var target = $(this).data("target");
-
+    function activateTab(target) {
         $(".tab").removeClass("active");
-        $(this).addClass("active");
+        $('.tab[data-target="' + target + '"]').addClass("active");
 
         $(".case").removeClass("active");
         $("." + target).addClass("active");
+
+        // 讓頁面滾回頂部
+        window.scrollTo(0, 0);
+    }
+
+    // 監聽 .tab 點擊事件
+    $(".tab").click(function () {
+        var target = $(this).data("target");
+        activateTab(target);
+    });
+
+    // 監聽 .next_btn a 點擊事件
+    $(".next_btn a").click(function (e) {
+        e.preventDefault(); // 阻止預設行為
+        var target = $(this).data("target");
+        activateTab(target);
     });
 });
